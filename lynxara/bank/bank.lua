47be7e483f68e5634f2fb40de976052e6dfaa666
LynxarA <lynxaradev@gmail.com>
2024-12-19 20:15:22 +0100

    2.0

--- lynxara/bank/bank.lua
+++ lynxara/bank/bank.lua
@@ -5,22 +5,24 @@ local storage = peripheral.find("sophisticatedstorage:iron_chest")
 local drive = peripheral.find("drive")
 
 local balance = 0
+local depositAmount = 0
 
 local Encryption = {}
-Encryption.Encrypt = function(text, key)
+local key = { 77, 30, 36, 18, 39, 34, 49, 56, 44, 19, 99, 97, 77, 29, 59, 100, 72, 62, 1, 1, 15, 58, 51, 5, 7, 79, 50, 3, 40, 85, 0, 87 }
+Encryption.Encrypt = function(text)
     local encrypted = ""
     for i = 1, string.len(text) do
         local char = string.byte(text, i)
-        encrypted[i] = string.char(char + key[i % #key])
+        encrypted = encrypted .. string.char(char + key[i % #key])
     end
     return encrypted
 end
 
-Encryption.Decrypt = function(text, key)
+Encryption.Decrypt = function(text)
     local decrypted = ""
     for i = 1, string.len(text) do
         local char = string.byte(text, i)
-        decrypted[i] = string.char(char - key[i % #key])
+        decrypted = decrypted .. string.char(char - key[i % #key])
     end
     return decrypted
 end
@@ -50,19 +52,93 @@ if EverythingExists() == false then
     return
 end
 
+local function BlinkButton(type, state)
+    if type == "deposit" then
+        local width, height = monitor.getSize()
+        if state == true then 
+            monitor.setBackgroundColor(colors.blue)
+            monitor.setCursorPos(5, 15)
+            monitor.setTextColor(colors.white)
+            monitor.write(string.rep(" ", width - 8))
+            monitor.setCursorPos(math.floor(width / 2 - string.len("Deposit") / 2) + 1, 15)
+            monitor.write("Deposit")
+            sleep(1)
+            monitor.setBackgroundColor(colors.green)
+            monitor.setCursorPos(5, 15)
+            monitor.setTextColor(colors.white)
+            monitor.write(string.rep(" ", width - 8))
+            monitor.setCursorPos(math.floor(width / 2 - string.len("Deposit") / 2) + 1, 15)
+            monitor.write("Deposit")
+        else 
+            monitor.setBackgroundColor(colors.red)
+            monitor.setCursorPos(5, 15)
+            monitor.setTextColor(colors.white)
+            monitor.write(string.rep(" ", width - 8))
+            monitor.setCursorPos(math.floor(width / 2 - string.len("Deposit") / 2) + 1, 15)
+            monitor.write("Deposit")
+            sleep(1)
+            monitor.setBackgroundColor(colors.green)
+            monitor.setCursorPos(5, 15)
+            monitor.setTextColor(colors.white)
+            monitor.write(string.rep(" ", width - 8))
+            monitor.setCursorPos(math.floor(width / 2 - string.len("Deposit") / 2) + 1, 15)
+            monitor.write("Deposit")
+        end
+    elseif type == "convert" then
+        if state == true then
+            monitor.setBackgroundColor(colors.green)
+            monitor.setCursorPos(24, 5)
+            monitor.write("            ")
+            monitor.setTextColor(colors.white)
+            monitor.setCursorPos(24, 6)
+            monitor.write("Convert Disk")
+            monitor.setCursorPos(24, 7)
+            monitor.write("            ")
+            sleep(1)
+            monitor.setBackgroundColor(colors.blue)
+            monitor.setCursorPos(24, 5)
+            monitor.write("            ")
+            monitor.setTextColor(colors.white)
+            monitor.setCursorPos(24, 6)
+            monitor.write("Convert Disk")
+            monitor.setCursorPos(24, 7)
+            monitor.write("            ")
+        else
+            monitor.setBackgroundColor(colors.red)
+            monitor.setCursorPos(24, 5)
+            monitor.write("            ")
+            monitor.setTextColor(colors.white)
+            monitor.setCursorPos(24, 6)
+            monitor.write("Convert Disk")
+            monitor.setCursorPos(24, 7)
+            monitor.write("            ")
+            sleep(1)
+            monitor.setBackgroundColor(colors.blue)
+            monitor.setCursorPos(24, 5)
+            monitor.write("            ")
+            monitor.setTextColor(colors.white)
+            monitor.setCursorPos(24, 6)
+            monitor.write("Convert Disk")
+            monitor.setCursorPos(24, 7)
+            monitor.write("            ")
+        end
+    end
+end
+
 local Pages = {}
 Pages.Main = function()
     monitor.setBackgroundColor(colors.black)
     monitor.clear()
 
     local width, height = monitor.getSize()
-    local labelText = "PyroTrain Menu"
+    local labelText = "PyroTrain"
+
 
     -- Title Label
     monitor.setCursorPos(math.floor(width / 2 - string.len(labelText) / 2) + 1, 1)
     monitor.setTextColor(colors.yellow)
     monitor.write(labelText)
-    
+
     -- Border
     monitor.setBackgroundColor(colors.gray)
     monitor.setCursorPos(2, 2)
@@ -84,6 +160,7 @@ Pages.Main = function()
     monitor.write("  ")
     monitor.setBackgroundColor(colors.black)
     monitor.write(" ")
+    monitor.setTextColor(colors.yellow)
     monitor.write(diTitle)
     monitor.write(" ")
     monitor.setBackgroundColor(colors.gray)
@@ -97,11 +174,11 @@ Pages.Main = function()
     monitor.setCursorPos(4, 8)
     monitor.write("                  ")
     monitor.setCursorPos(6, 6)
-    if drive.isDiskPresent() == true then 
+    if drive.isDiskPresent() == true then
         monitor.setBackgroundColor(colors.green)
         monitor.setTextColor(colors.white)
         monitor.write("   Inserted   ")
-    else 
+    else
         monitor.setBackgroundColor(colors.red)
         monitor.setTextColor(colors.black)
         monitor.write(" Not Inserted ")
@@ -128,18 +205,63 @@ Pages.Main = function()
     monitor.setTextColor(colors.white)
     monitor.setCursorPos(24, 6)
     monitor.write("Convert Disk")
-end
 
-Pages.Reset = function()
+    -- Deposit Module
+    -- Border all width
+    local depositTitle = " Deposit "
+    monitor.setBackgroundColor(colors.gray)
+    monitor.setTextColor(colors.yellow)
+    monitor.setCursorPos(4, 10)
+    monitor.write(string.rep(" ", width / 2 - string.len(depositTitle) / 2 - 3))
     monitor.setBackgroundColor(colors.black)
-    monitor.clear()
+    monitor.write(depositTitle)
+    monitor.setBackgroundColor(colors.gray)
+    monitor.write(string.rep(" ", width / 2 - string.len(depositTitle) / 2 - 3))
+    for i = 1, 5 do
+        monitor.setCursorPos(4, 10 + i)
+        monitor.write(" ")
+        monitor.setCursorPos(width - 3, 10 + i)
+        monitor.write(" ")
+    end
+    monitor.setCursorPos(4, 14)
+    monitor.write(string.rep(" ", width - 6))
+    monitor.setCursorPos(4, 16)
+    monitor.write(string.rep(" ", width - 6))
 
-    -- Back Button
-    monitor.setCursorPos(1, 1)
+    -- Money Label
+    if drive.isDiskPresent() == true then
+        local data = fs.open("disk/data", "r")
+        local encryptedData = data.readAll()
+        data.close()
+        local decryptedData = Encryption.Decrypt(encryptedData)
+        local data = textutils.unserializeJSON(decryptedData)
+        balance = data.money
+    else
+        balance = 0
+    end
+
+    local moneyLabel = "Money: " .. balance .. "C | Deposit: 0C"
+    monitor.setCursorPos(math.floor(width / 2 - string.len(moneyLabel) / 2) + 1, 12)
+    monitor.setBackgroundColor(colors.black)
     monitor.setTextColor(colors.white)
-    monitor.setBackgroundColor(colors.red)
-    monitor.write("Back")
+    monitor.write("Money: ")
+    monitor.setTextColor(colors.green)
+    monitor.write(balance)
+    monitor.setTextColor(colors.white)
+    monitor.write("C | Deposit: ")
+    monitor.setTextColor(colors.green)
+    monitor.write(depositAmount)
+    monitor.setTextColor(colors.white)
+    monitor.write("C")
 
+    -- Deposit Button
+    local depositText = "Deposit"
+    monitor.setCursorPos(5, 15)
+    monitor.setBackgroundColor(colors.green)
+    monitor.setTextColor(colors.white)
+    monitor.write(string.rep(" ", width - 8))
+    monitor.setCursorPos(math.floor(width / 2 - string.len(depositText) / 2) + 1, 15)
+    monitor.write(depositText)
 end
 
 Pages.Deposit = function()
@@ -153,26 +275,63 @@ Pages.Deposit = function()
     monitor.write("Back")
 end
 
+local function CheckMoney()
+    local moneyTypes = {
+        [1] = {
+            name = "numismatics:spur",
+            value = 1
+        },
+        [2] = {
+            name = "numismatics:bevel",
+            value = 8
+        },
+        [3] = {
+            name = "numismatics:sprocket",
+            value = 16
+        },
+        [4] = {
+            name = "numismatics:cog",
+            value = 64
+        },
+        [5] = {
+            name = "numismatics:crown",
+            value = 512
+        },
+        [6] = {
+            name = "numismatics:sun",
+            value = 4096
+        }
+    }
+
+    depositAmount = 0
+    local items = deposit.list()
+    if items[2] ~= nil then
+        for i = 1, #moneyTypes do
+            if items[2].name == moneyTypes[i].name then
+                depositAmount = depositAmount + items[2].count * moneyTypes[i].value
+            end
+        end
+    end
+end
+
 local status = "main"
 local previousStatus = "main"
 local driveStatus = false
 local function Update()
     Pages.Main()
-    while true do 
+    while true do
         sleep(0.1)
-        if status == "main" and previousStatus ~= "main" then 
+        CheckMoney()
+        if status == "main" and previousStatus ~= "main" then
             Pages.Main()
             previousStatus = "main"
-        elseif status == "reset" and previousStatus ~= "reset" then 
-            Pages.Reset()
-            previousStatus = "reset"
         elseif status == "deposit" and previousStatus ~= "deposit" then
             Pages.Deposit()
             previousStatus = "deposit"
         end
 
-        if status == "main" then 
-            if driveStatus ~= drive.isDiskPresent() then 
+        if status == "main" then
+            if driveStatus ~= drive.isDiskPresent() then
                 previousStatus = "none"
             end
         end
@@ -180,12 +339,26 @@ local function Update()
 end
 
 local Buttons = {}
-Buttons.Reset = function()
-    status = "reset"
-end
 
 Buttons.Deposit = function()
-    status = "deposit"
+    if depositAmount > 0 and drive.isDiskPresent() then
+        local tempAmount = depositAmount
+        storage.pullItems(peripheral.getName(deposit), 2)
+        depositAmount = 0
+        balance = balance + tempAmount
+        local data = { money = balance }
+        local encryptedData = textutils.serializeJSON(data)
+        encryptedData = Encryption.Encrypt(encryptedData)
+        if fs.exists("disk/data") == true then
+            fs.delete("disk/data")
+        end
+        local file = fs.open("disk/data", "w")
+        file.write(encryptedData)
+        file.close()
+        BlinkButton("deposit", true)
+    else
+        BlinkButton("deposit", false)
+    end
 end
 
 Buttons.Back = function()
@@ -193,76 +366,45 @@ Buttons.Back = function()
 end
 
 Buttons.Convert = function()
-    local success = false 
+    local success = false
 
-    if drive.isDiskPresent() == true then 
-        balance = 0 
-        local data = {money = balance}
+    if drive.isDiskPresent() == true then
+        balance = 0
+        local data = { money = balance }
         local encryptedData = textutils.serializeJSON(data)
-        local key = {77, 30, 36, 18, 39, 34, 49, 56, 44, 19, 99, 97, 77, 29, 59, 100, 72, 62, 1, 1, 15, 58, 51, 5, 7, 79, 50, 3, 40, 85, 0, 87}
-        encryptedData = Encryption.Encrypt(encryptedData, key)
-        if fs.exists("disk/data") == true then 
+        encryptedData = Encryption.Encrypt(encryptedData)
+        if fs.exists("disk/data") == true then
             fs.delete("disk/data")
         end
         local file = fs.open("disk/data", "w")
         file.write(encryptedData)
         file.close()
         status = "none"
-        monitor.setBackgroundColor(colors.green)
-        monitor.setCursorPos(24, 5)
-        monitor.write("            ")
-        monitor.setTextColor(colors.white)
-        monitor.setCursorPos(24, 6)
-        monitor.write("Convert Disk")
-        monitor.setCursorPos(24, 7)
-        monitor.write("            ")
-        sleep(1)
-        monitor.setBackgroundColor(colors.blue)
-        monitor.setCursorPos(24, 5)
-        monitor.write("            ")
-        monitor.setTextColor(colors.white)
-        monitor.setCursorPos(24, 6)
-        monitor.write("Convert Disk")
-        monitor.setCursorPos(24, 7)
-        monitor.write("            ")
+        BlinkButton("convert", true)
+        status = "main"
         previousStatus = "none"
-    else 
-        monitor.setBackgroundColor(colors.red)
-        monitor.setCursorPos(24, 5)
-        monitor.write("            ")
-        monitor.setTextColor(colors.white)
-        monitor.setCursorPos(24, 6)
-        monitor.write("Convert Disk")
-        monitor.setCursorPos(24, 7)
-        monitor.write("            ")
-        sleep(1)
-        monitor.setBackgroundColor(colors.blue)
-        monitor.setCursorPos(24, 5)
-        monitor.write("            ")
-        monitor.setTextColor(colors.white)
-        monitor.setCursorPos(24, 6)
-        monitor.write("Convert Disk")
-        monitor.setCursorPos(24, 7)
-        monitor.write("            ")
+    else
+        BlinkButton("convert", false)
+        status = "main"
         previousStatus = "none"
     end
 end
 
 local function EventCheck()
-    while true do 
+    while true do
         sleep(0.1)
-        
+
         local event, side, xPos, yPos = os.pullEvent("monitor_touch")
-        if status == "main" then 
-            if yPos == 6 and (xPos > 0 and xPos < 11) then 
+        if status == "main" then
+            if yPos == 6 and (xPos > 0 and xPos < 11) then
                 Buttons.Reset()
-            elseif yPos == 8 and (xPos > 0 and xPos < 8) then 
+            elseif (yPos > 14 and yPos < 16) and (xPos > 4 and xPos < 36) then
                 Buttons.Deposit()
-            elseif (yPos > 4 and yPos < 8) and (xPos > 23 and xPos < 36) then 
+            elseif (yPos > 4 and yPos < 8) and (xPos > 23 and xPos < 36) then
                 Buttons.Convert()
             end
-        elseif status ~= "main" then 
-            if yPos == 1 and (xPos > 0 and xPos < 5) then 
+        elseif status ~= "main" then
+            if yPos == 1 and (xPos > 0 and xPos < 5) then
                 Buttons.Back()
             end
         end
